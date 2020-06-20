754d3bad6e42eb8eac80168f852b45836314bc87
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-06-20 23:02:52 +0400

    Update Backup_app.py

--- Backup_app.py
+++ Backup_app.py
@@ -2,32 +2,47 @@ import os
 import time
 import zipfile
 
-if os.sep == '\\':
-    source = f'C:\\Users\\{os.getlogin()}\\Documents'
-else:
-    source = f'/home/{os.getlogin()}/Documents'
 
-if os.sep == '\\':
-    target_dir = 'E:\\Backup'
-else:
-    target_dir = f'/home/{os.getlogin()}/Backup'
+def src_for_win():
+    return f'C:\\Users\\{os.getlogin()}\\Documents'
 
-today = target_dir + os.sep + time.strftime('%d_%m_%Y')
-now = time.strftime('%H_%M_%S')
 
-comment = input('Enter comment: --> ')
-if len(comment) == 0:
-    target = today + os.sep + now + '.zip'
-else:
-    target = (today + os.sep + now + '_' +
-              comment.replace(' ', '_') + '.zip')
-            
-# Make catalog if not exists.
-if not os.path.exists(today):
-    os.makedirs(today)
-    print(f'The catalogue <<{today}>> was successfully created')
+def src_for_unix():
+    return f'/home/{os.getlogin()}/Documents'
 
-with zipfile.ZipFile(target, 'w') as zip_com:
-    for root, dirs, files in os.walk(source):
-        for file_n in files:
-            zip_com.write(os.path.join(root, file_n))
+
+def tar_dir_win():
+    return 'E:\\Backup'
+
+
+def tar_dir_unix():
+    return f'/home/{os.getlogin()}/Backup'
+
+
+def main_func():
+    if os.sep == '\\':
+        source = src_for_win()
+        target_dir = tar_dir_win()
+    else:
+        source = src_for_unix()
+        target_dir = tar_dir_unix()
+
+    today = target_dir + os.sep + time.strftime('%d_%m_%Y')
+    now = time.strftime('%H_%M_%S')
+    comment = input('Enter comment: --> ')
+    if len(comment) == 0:
+        target = today + os.sep + now + '.zip'
+    else:
+        target = (today + os.sep + now + '_' +
+                  comment.replace(' ', '_') + '.zip')
+    if not os.path.exists(today):
+        os.makedirs(today)
+        print(f'The catalogue <<{today}>> was successfully created')
+    with zipfile.ZipFile(target, 'w') as zip_com:
+        for root, dirs, files in os.walk(source):
+            for file_n in files:
+                zip_com.write(os.path.join(root, file_n))
+
+
+if __name__ == '__main__':
+    main_func()
