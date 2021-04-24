243a062fa156a63b49bc176cccb3cf714e3497cf
SaidRem <said.r@yandex.ru>
2021-04-24 22:36:26 +0400

    Added func for later implementation

---
+++ copy_file_tree/copy_files.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+ONE_MB = 2**20  # 1 Mb
+HUNDR_MB = ONE_MB * 100  # 100 Mb 
+
+
+def file_copy(orig_file, copy_to, file_size=HUNDR_MB):
+    """
+    Copy a file from 'orig_file' (path) to directory 'copy_to' (path).
+    """
+    pass
+
+
+def file_tree_cp(orig_dir, copy_to):
+    """
+    Copy all dirs, subdirs and files in 'orig_dir'
+    to 'copy_to' (path to empty dir).
+    """
+    pass
+
