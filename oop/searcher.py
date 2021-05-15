c149ce386d8ccdb0ccb1c2f3b991a00292053942
SaidRem <said.r@yandex.ru>
2021-05-15 22:35:18 +0400

    Added oop/searcher

---
+++ oop/searcher.py
@@ -0,0 +1,13 @@
+# Classes for wrap some of the details of os.walk
+# to walk and search
+
+import os
+import sys
+
+class FileTraveler:
+    """
+    Iterates over all files in all
+    folders below cur_dir in 'run'
+    method.
+    """
+    pass
