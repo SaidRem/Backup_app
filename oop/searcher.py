3b45eb132ba761f9bbb7a9d139fa514a433fa220
SaidRem <said.r@yandex.ru>
2021-05-15 22:41:51 +0400

    Added init, run

--- oop/searcher.py
+++ oop/searcher.py
@@ -7,7 +7,16 @@ import sys
 class FileTraveler:
     """
     Iterates over all files in all
-    folders below cur_dir in 'run'
+    folders below start_dir in 'run'
     method.
     """
-    pass
+    def __init__(self, search_word=None):
+        self.files_cnt = 0
+        self.dirs_cnt = 0
+        self.search_word = search_word
+
+    def run(self, start_dir=os.curdir):
+        self.reset()                     # reset counter for reuse
+        for cur_dir, sub_dirs, files_list in os.walk(start_dir):
+            self.visitdir(cur_dir)
+
