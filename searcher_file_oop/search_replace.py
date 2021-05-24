3fc8169e55fe2d6468d34f76f927c2d03949b20a
SaidRem <said.r@yandex.ru>
2021-05-24 22:08:06 +0400

    Added file

---
+++ searcher_file_oop/search_replace.py
@@ -0,0 +1,13 @@
+"""
+Search and replace in all files in a directory tree.
+"""
+
+import sys
+from searcher import SearchInFiles
+
+
+class SearchReplace(SearchInFiles):
+    """
+    Replace string in file tree by custom string.
+    """
+    pass 
