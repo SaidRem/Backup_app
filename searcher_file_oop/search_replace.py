c2616aae60c7fea383e1716d9916301a62db65cc
SaidRem <said.r@yandex.ru>
2021-05-24 22:15:44 +0400

    Added new methods for replace in strings in files

--- searcher_file_oop/search_replace.py
+++ searcher_file_oop/search_replace.py
@@ -10,4 +10,12 @@ class SearchReplace(SearchInFiles):
     """
     Replace string in file tree by custom string.
     """
-    pass 
+    def __init__(self, old_str, new_str):
+        self.old_str = old_str
+        self.new_str = new_str
+        SearchInFiles.__init__(self, old_str)
+
+    def word_matched(self):
+        """
+        Replace self.old_str by self.new_str.
+        """
