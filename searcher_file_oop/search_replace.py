9d5320ef2fd30a2ce4fe0f8fd0f195d9b74cc96c
SaidRem <said.r@yandex.ru>
2021-05-25 21:55:48 +0400

    Added comments to methods of class SearchReplace

--- searcher_file_oop/search_replace.py
+++ searcher_file_oop/search_replace.py
@@ -9,6 +9,8 @@ from searcher import SearchInFiles
 class SearchReplace(SearchInFiles):
     """
     Replace string in file tree by custom string.
+    old_str - replacement string is passed to the constructor
+    of class SearchInFiles (as "search_word").
     """
     def __init__(self, old_str, new_str):
         self.replaced = []
@@ -18,8 +20,9 @@ class SearchReplace(SearchInFiles):
     def word_matched(self, full_path, text):
         """
         Replace self.old_str by self.new_str.
+        text - from visit_file method of SearchInFiles class.
         """
-        old_str, new_str = self.search_word, self..new_str
+        old_str, new_str = self.search_word, self.new_str
         text = text.replace(old_str, new_str)
         open(full_path, 'w').write(text)
 
