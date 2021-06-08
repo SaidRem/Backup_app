9ca139fe03616474418a9428792c98e5a6b24dda
SaidRem <said.r@yandex.ru>
2021-06-08 22:35:39 +0400

    Added sys.argv and input lines, comments

--- searcher_file_oop/copy_all.py
+++ searcher_file_oop/copy_all.py
@@ -3,6 +3,7 @@ Copying file tree using classes.
 """
 
 import os
+import sys
 from pathlib import Path
 from searcher import FileTraveler
 from ..copy_file_tree.copy_files import copy_file
@@ -13,6 +14,12 @@ class CopyFileTree(FileTraveler):
     """
     
     def __init__(self, orig_dir, copy_dir, trace=1):
+        """
+        :param orig_dir: Path to the folder for copy.
+        :param copy_dir: Path to the folder for storage copy.
+        :param trace: Show progress: 1 - display visited folders;
+                                     2 - display visited files, folders.
+        """
         self.orig_dir_len = len(orig_dir) + 1
         self.copy_dir = copy_dir
         FileTraveler.__init__(self, trace=trace)
@@ -34,3 +41,14 @@ class CopyFileTree(FileTraveler):
             print('f', file_path, '=>', to_path)
         copy_file(file_path, to_path)
         self.files_cnt += 1
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        orig_dir, copy_dir = sys.argv[1:3]
+        obj = CopyFileTree(orig_dir, copy_dir)
+        obj.run(start_dir=orig_dir)
+    else:
+        orig_dir = input('Enter path to dir for copy')
+        copy_dir = input('Enter path to dir for storage')
+        obj = CopyFileTree(orig_dir, copy_dir)
+        obj.run(start_dir=orig_dir)
